2aa1fc5e881e5665be25b36c58ff61c60efde2a6
adam91holt <holta5@uni.coventry.ac.uk>
2015-02-19 18:48:46 +0000

    Wikipedia is working, we just need to add wikipedia id's to datastore and then push that page id through in the onclick on the get wiki button

--- js/wikipedia.js
+++ js/wikipedia.js
@@ -1,9 +1,5 @@
-
- 
-
-
 function getwiki(pageid){
-     var httpRequest, key = "oCdV3MjLj1mshtyIXwBVzBqRKtY9p1XJNiajsn1vsCETYVLwK3";
+   var httpRequest, key = "oCdV3MjLj1mshtyIXwBVzBqRKtY9p1XJNiajsn1vsCETYVLwK3";
    var url = "https://community-wikipedia.p.mashape.com/api.php?format=json&action=query&prop=extracts&pageids="+ pageid + "&redirects=true";
    return new Promise(function(resolve, reject) {
        if(window.XMLHttpRequest) { // mozilla, safari,...
@@ -16,40 +12,10 @@ function getwiki(pageid){
        httpRequest.onload = function() {
            if(httpRequest.readyState === 4 && httpRequest.status === 200) {
                var response = JSON.parse(httpRequest.responseText);
-               console.log(response['query']['pages'][pageid])
+               var div = document.getElementById('wikiInfo')
+               div.innerHTML = response['query']['pages'][pageid]['extract']
            }
        };
        httpRequest.send();
-   });
-    
-    
+   });  
 }
-
-
-function init(){
-    console.log(document.getElementById('getteam'))
-//    document.getElementById('getteam').addEventListener("click", function(){
-//        getwiki('Arsenal_F.C')
-//    })
-} 
-// var displayItems = function(response) {
-//    'use strict';
-//    var i,
-//        newlist = document.createElement('UL'),
-//        div = document.getElementById('suggestions'),
-//        newitem = '',
-//        text = '',
-//        link = '';
-//    response.forEach(function(item) {
-//        text = document.createTextNode(item);
-//        newitem = document.createElement('li');
-//        link = document.createElement('a');
-//        link.href = "http://en.wikipedia.org/wiki/" + item.replace(/ /g, "_");
-//        link.appendChild(text);
-//        newitem.appendChild(link);
-//        newlist.appendChild(newitem);
-//    });
-//    div.appendChild(newlist);
-// };
- 
-window.onload = init();
