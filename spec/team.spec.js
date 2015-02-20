c1952f19e31f3a974bb73f185739def31633d358
reissefer <reissefer@gmail.com>
2015-02-20 13:43:00 +0000

    zombie is now at a good point just need to add more tests

--- spec/team.spec.js
+++ spec/team.spec.js
@@ -1,39 +1,25 @@
 var Browser = require("zombie");
 var url = "http://mashup321com.appspot.com/";
 var browser = new Browser();
-describe("Homepage", function() {
-    describe('browser', function(){
+jasmine.getEnv().defaultTimeoutInterval = 999999
+describe("Homepage : ", function() {
+    describe('browser : ', function() {
         it("should have defined headless browser", function(next) {
-        expect(typeof browser != "undefined").toBe(true);
-        expect(browser instanceof Browser).toBe(true);
-        next();
-    }); 
-        
-        
+            expect(typeof browser != "undefined").toBe(true);
+            expect(browser instanceof Browser).toBe(true);
+            next();
+        });
     })
-    
-     describe('checking the pageage', function(){
-      it("should visit the site and see the login form", function(next, done) {
-        browser.visit(url, function(err) {
-            expect(browser.success).toBe(true);
-            expect(browser.query("input[value='test']")).toBeDefined(); 
-        }).then(function(){
-            browser.assert.element('#teamCon');
-            browser.assert.text('title', 'Football Mashup');
-            console.log('passed the 1st title')
-            browser.assert.text('title', 'random', 'wrong ');
-            //next();
-            done();
-        }).fail(function(err){
-            console.log(err);
-            done(err);
-        })
-    });  
-        
-        
+    describe('checking the page', function() {
+        it("should visit the site and see the login form", function(done) {
+            browser.visit(url).then(function(done) {
+                browser.assert.element('#teamCon');
+                browser.assert.text('title', 'Football Mashup');
+            }).then(function(done) {
+                browser.assert.text('title', 'random', 'Wrong title');
+            }).then(done, done)
+        });
     })
-   
-    
 });
 // describe("Test Javascript", function() {
 //     it("should have defined headless browser", function(next) {
@@ -50,7 +36,6 @@ describe("Homepage", function() {
 //         });
 //         next();
 //     });
-    
 //      it("it should be able to press a team", function(next) {
 //         browser.visit(url, function(e, browser) {
 //             assert.ok(browser.success);
@@ -60,12 +45,15 @@ describe("Homepage", function() {
 //         });
 //         next();
 //     });
-    
-    
-    
 //     .pressButton('input[value="Login"]', function() {
 //             expect(browser.html("body")).not.toContain("Insanely fast, headless full-stack testing using Node.js");
 //             expect(browser.query("input[value='Login']")).toBeDefined();
 //             next();
 //         });
 //});
+//
+//
+//function(err) {
+//             expect(browser.success).toBe(true);
+//             expect(browser.query("input[value='test']")).toBeDefined(); 
+//         }
