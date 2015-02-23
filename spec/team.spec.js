48ba9f2132cee09e715f000ef2cdc3c96943bb07
reissefer <reissefer@gmail.com>
2015-02-23 14:49:49 +0000

    changes to zombie spec

--- spec/team.spec.js
+++ spec/team.spec.js
@@ -11,49 +11,20 @@ describe("Homepage : ", function() {
         });
     })
     describe('checking the page', function() {
-        it("should visit the site and see the login form", function(done) {
+        it("The page should show the title", function(done) {
             browser.visit(url).then(function(done) {
                 browser.assert.element('#teamCon');
-                browser.assert.text('title', 'Football Mashup');
+                browser.assert.text('title', 'Football Mashup', 'wrong page title');
             }).then(function(done) {
-                browser.assert.text('title', 'random', 'Wrong title');
+                return browser.clickLink('#details_Chelsea')
+            }).then(function(done) {
+                browser.wait;
+                browser.wait(pageLoaded, function() {
+                    console.log(browser.html());
+                });
+                //browser.assert.success()
+                browser.assert.text('title', 'Chelsea', 'wrong page title');
             }).then(done, done)
         });
     })
 });
-// describe("Test Javascript", function() {
-//     it("should have defined headless browser", function(next) {
-//         expect(typeof browser != "undefined").toBe(true);
-//         expect(browser instanceof Browser).toBe(true);
-//         next();
-//     });
-//     it("should visit the site and see the teams", function(next) {
-//         browser.visit(url, function(e, browser) {
-//             assert.ok(browser.success);
-//             if(browser.error) {
-//                 console.dir("Errors reported:", browser.errors)
-//             };
-//         });
-//         next();
-//     });
-//      it("it should be able to press a team", function(next) {
-//         browser.visit(url, function(e, browser) {
-//             assert.ok(browser.success);
-//             if(browser.error) {
-//                 console.dir("Errors reported:", browser.errors)
-//             };
-//         });
-//         next();
-//     });
-//     .pressButton('input[value="Login"]', function() {
-//             expect(browser.html("body")).not.toContain("Insanely fast, headless full-stack testing using Node.js");
-//             expect(browser.query("input[value='Login']")).toBeDefined();
-//             next();
-//         });
-//});
-//
-//
-//function(err) {
-//             expect(browser.success).toBe(true);
-//             expect(browser.query("input[value='test']")).toBeDefined(); 
-//         }
