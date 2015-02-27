9738a0409b882884b580006667d45b2c9ae7c9fa
adam91holt <holta5@uni.coventry.ac.uk>
2015-02-27 10:23:18 +0000

    Used the new football-data API token

--- teams.py
+++ teams.py
@@ -1,23 +1,30 @@
 import urllib2, json
 
+API_TOKEN = 'c352b328988b454483566956e6882442'
+
 def allTeams():
     #Actual API request
-    data = urllib2.urlopen('http://api.football-data.org/alpha/soccerseasons/354/leagueTable').read()
+    req = urllib2.Request('http://api.football-data.org/alpha/soccerseasons/354/leagueTable')
+    req.add_header('X-Auth-Token', API_TOKEN)
+    resp = urllib2.urlopen(req)
+    content = resp.read()
     #Decode to JSON                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-    dataDecode = json.loads(data.decode('utf8'))    
+    dataDecode = json.loads(content.decode('utf8'))    
     #Store all teams
     teams = dataDecode['standing']
     return teams
-    print teams
 
 
 
 def tableStandings():
     #URL for getting all teams     
-    #urllib2.urlopen                                                                                                                                                                                                                         
-    data = urllib2.urlopen('http://api.football-data.org/alpha/soccerseasons/354/leagueTable').read()
-    #Variables to get to where is needed                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-    dataDecode = json.loads(data.decode('utf8'))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+    #Actual API request
+    req = urllib2.Request('http://api.football-data.org/alpha/soccerseasons/354/leagueTable')
+    req.add_header('X-Auth-Token', API_TOKEN)
+    resp = urllib2.urlopen(req)
+    content = resp.read()
+    #Decode to JSON                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    dataDecode = json.loads(content.decode('utf8'))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
     teamlist = dataDecode['standing']                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
     #Stores all teams and positions
     allteams = []
@@ -44,9 +51,12 @@ def teamList(arg):
             #Get the url for the club
             cluburl = t['_links']['team']['href']
             #Open up the url
-            openclub = urllib2.urlopen(cluburl).read()
+            req = urllib2.Request(cluburl)
+            req.add_header('X-Auth-Token', API_TOKEN)
+            resp = urllib2.urlopen(req)
+            content = resp.read()
             #Convert JSON
-            clubJSON = json.loads(openclub.decode('utf8')) 
+            clubJSON = json.loads(content.decode('utf8')) 
             #Players URL
             playersurl = clubJSON['_links']['players']['href']
             #Get all players
